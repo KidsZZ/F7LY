b40ce684543a06611cc7116b86564995ebb70fe6
gkq <guankaiqi@whu.edu.cn>
2025-07-15 16:26:41 +0800

    getdents

--- kernel/fs/vfs/vfs_utils.hh
+++ kernel/fs/vfs/vfs_utils.hh
@@ -7,3 +7,4 @@ int vfs_path2filetype(eastl::string &absolute_path);
 int create_and_write_file(const char *path, const char *data);
 int is_file_exist(const char *path);
 uint vfs_read_file(const char *path, uint64 buffer_addr, size_t offset, size_t size);
+int vfs_getdents(fs::file *&file, struct linux_dirent64 *dirp, uint count);
